e1dd696f27b9bf1f95c379a91904b5f52de53ebb
Patrick L Archibald <patrick.archibald@gmail.com>
2016-01-18 10:10:01 -0500

    Initial commit.

---
+++ src/com/pla/misc/PingImageServlet.java
@@ -0,0 +1,90 @@
+package com.pla.misc;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Map.Entry;
+
+import javax.imageio.ImageIO;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class PingImageServlet extends HttpServlet {
+
+  private static final long serialVersionUID = 1772025897783209959L;
+  private final int WIDTH = 10;
+  private final int HEIGHT = 10;
+
+  protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    doGet(request, response);
+  }
+
+  protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    String ip = null;
+    for (Entry<String, String[]> entry : request.getParameterMap().entrySet()) {
+      String[] values = entry.getValue();
+      if (values.length > 0) {
+        ip = values[0];
+      }
+    }
+    if (isBlank(ip)) {
+      response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Missing ip address parameter.");
+      return;
+    }
+    response.setContentType("image/png");
+    Rectangle rectangle = new Rectangle(0, 0, WIDTH, HEIGHT);
+    BufferedImage bufferedImage = new BufferedImage(rectangle.width, rectangle.height, BufferedImage.TYPE_INT_RGB);
+    Graphics2D g2d = bufferedImage.createGraphics();
+    if (isAlive(ip)) {
+      g2d.setColor(Color.GREEN);
+    } else {
+      g2d.setColor(Color.RED);
+    }
+    g2d.fill(rectangle);
+    ImageIO.write(bufferedImage, "png", response.getOutputStream());
+  }
+
+  private boolean isBlank(String s) {
+    if (s == null || s.trim().length() == 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  private boolean isAlive(String ipAddress) {
+    if (isBlank(ipAddress)) {
+      return false;
+    }
+    boolean result = false;
+    String cmd = "ping -c 1 -W 1 " + ipAddress;
+    BufferedReader bufferedReader = null;
+    try {
+      Runtime runtime = Runtime.getRuntime();
+      Process process = runtime.exec(cmd);
+      bufferedReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+      String line;
+      while ((line = bufferedReader.readLine()) != null) {
+        if (line.indexOf("(0% loss)") != -1 || line.indexOf("successful (100 %)") != -1 || line.indexOf(" 0% packet loss,") != -1) {
+          result = true;
+        }
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    } finally {
+      if (bufferedReader != null) {
+        try {
+          bufferedReader.close();
+        } catch (IOException e) {
+        }
+      }
+    }
+    return result;
+  }
+}
---
+++ src/com/pla/misc/TimeImageServlet.java
@@ -0,0 +1,53 @@
+package com.pla.misc;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.RenderingHints;
+import java.awt.font.FontRenderContext;
+import java.awt.geom.AffineTransform;
+import java.awt.image.BufferedImage;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.imageio.ImageIO;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class TimeImageServlet extends HttpServlet {
+  private static final long serialVersionUID = 1838634471441519824L;
+  private final int WIDTH = 1920;
+  private final int HEIGHT = 1080;
+
+  protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    response.setHeader("Refresh", "60");
+    Rectangle rectangle = new Rectangle(0, 0, WIDTH, HEIGHT);
+    BufferedImage bufferedImage = new BufferedImage(rectangle.width, rectangle.height, BufferedImage.TYPE_INT_RGB);
+    Graphics2D g2d = bufferedImage.createGraphics();
+    g2d.setBackground(Color.WHITE);
+    g2d.fill(rectangle);
+    g2d.setColor(Color.BLACK);
+    g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+    Font font = new Font("Arial", Font.BOLD, 130);
+    g2d.setFont(font);
+    AffineTransform affinetransform = new AffineTransform();
+    FontRenderContext frc = new FontRenderContext(affinetransform, true, true);
+    Date date = new Date();
+    SimpleDateFormat dateFormat = new SimpleDateFormat("EEEEE MMMMM dd, yyyy");
+    SimpleDateFormat timeFormat = new SimpleDateFormat("h:mm a z");
+    String dateString = dateFormat.format(date);
+    String timeString = timeFormat.format(date);
+    int textheight = (int) (font.getStringBounds(dateString, frc).getHeight());
+    int textwidth = (int) (font.getStringBounds(dateString, frc).getWidth());
+    g2d.drawString(dateString, (WIDTH - textwidth) / 2, (HEIGHT - textheight) / 2);
+    textwidth = (int) (font.getStringBounds(timeString, frc).getWidth());
+    g2d.drawString(timeString, (WIDTH - textwidth) / 2, ((HEIGHT - textheight) / 2) + (int) (textheight * 1.2));
+    response.setContentType("image/png");
+    ImageIO.write(bufferedImage, "png", response.getOutputStream());
+  }
+}
